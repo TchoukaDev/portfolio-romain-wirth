c04dc07ed2848e4bd0480471574b72364c4e6b89
Romain Wirth <romain.wirth@gmail.com>
2025-11-19 11:14:14 +0100

    feat modification About provisoire

--- src/components/Sections/About/About.jsx
+++ src/components/Sections/About/About.jsx
@@ -4,16 +4,21 @@ export default function About() {
       <h2>A propos</h2>
       <div className="space-y-3">
         <p>
-          Développeur web passionné, je transforme les idées en sites modernes,
-          fluides et accessibles.
+          Développeur web en formation et passionné, je transforme les idées en
+          sites modernes, fluides et accessibles.
         </p>{" "}
         <p>
-          Curieux et persévérant, j’aime relever de nouveaux défis pour créer
+          Travaillant actuellement à l'hôpital, je projette d'effectuer une
+          reconversion dans le développement web. Ce site sera mis à jour
+          lorsque ce projet aura démarré.{" "}
+        </p>
+        <p>
+          {/* Curieux et persévérant, j’aime relever de nouveaux défis pour créer
           des expériences en ligne qui allient esthétique et performance.
         </p>{" "}
         <p>
           Issu du monde de la santé, j’ai à cœur de comprendre les besoins des
-          utilisateurs et de tout mettre en œuvre pour y répondre au mieux.
+          utilisateurs et de tout mettre en œuvre pour y répondre au mieux. */}
         </p>
       </div>
     </section>
